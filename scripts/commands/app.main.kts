a10638801811f55b2fede871215cf56cdf50f758
Edward Tanguay <edward@tanguay.info>
2025-04-19 19:12:53 +0200

    level deeper

--- scripts/commands/app.main.kts
+++
@@ -1,6 +0,0 @@
-@file:Import("../qtools/QDev.kts")
-
-import QDev.debug
-
-debug("inside create-page script")
-println("this script will create a new page in the React app")
---
+++ scripts/commands/create-page/app.main.kts
@@ -0,0 +1,6 @@
+@file:Import("../../qtools/QDev.kts")
+
+import QDev.debug
+
+debug("inside create-page script")
+println("this script will create a new page in the React application")
