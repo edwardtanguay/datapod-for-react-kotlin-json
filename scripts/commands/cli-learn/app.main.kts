9a58832e5e086f6e31fbbff111a34a0ddacd31ae
Edward Tanguay <edward@tanguay.info>
2025-04-26 20:33:54 +0200

    working 003

--- scripts/commands/cli-learn/app.main.kts
+++ scripts/commands/cli-learn/app.main.kts
@@ -1,26 +1,25 @@
-// @file:Import("../../qtools/qdev.kts")
-// @file:Import("../../qtools/qcli.kts")
-// @file:Import("../../developer/learn.kts")
+@file:Import("../../qtools/qdev.kts")
+@file:Import("../../qtools/qcli.kts")
+@file:Import("../../developer/learn.kts")
 
-// import qdev
-// import qcli
-// import learn
-// import kotlin.reflect.full.declaredFunctions
+import qdev
+import qcli
+import learn
+import kotlin.reflect.full.declaredFunctions
 
-// val exerciseNumber = args.getOrNull(0) ?: "001"
+val exerciseNumber = args.getOrNull(0) ?: "001"
 
-// qcli.message("Exercise Number: $exerciseNumber", "info")
+qcli.message("Exercise Number: $exerciseNumber", "info")
 
-// val methodName = "ex$exerciseNumber"
-// val method = learn::class.declaredFunctions.find { it.name == methodName }
+val methodName = "ex$exerciseNumber"
+val method = learn::class.declaredFunctions.find { it.name == methodName }
 
-// if (method != null) {
-//     method.call(learn)
-// } else {
-//     qcli.message("Method $methodName not found in learn", "error")
-// }
+if (method != null) {
+    method.call(learn)
+} else {
+    qcli.message("Method $methodName not found in learn", "error")
+}
 
-// // hack: suppress the "kotlin.Unit" (void) output
-// ""
-   
-println("testing") 
+// hack: suppress the "kotlin.Unit" (void) output
+""
+      
--- scripts/developer/learn.kts
+++ scripts/developer/learn.kts
@@ -1,7 +1,11 @@
-@file:Import("../../qtools/qcli.kts")
-
 object learn {
     fun ex001() {
         qcli.message("this is ex001", "info")
+    }
+    fun ex002() {
+        qcli.message("this is ex002", "info")
+    }
+    fun ex003() {
+        qcli.message("this is ex003", "info")
     }
 }
